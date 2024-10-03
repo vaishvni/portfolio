9bacd137b276909b5277910b9b02debeabf6e68c
vaishvni <vaishnavidoiphode029@gmail.com>
2024-10-03 15:48:47 +0530

    first commit

---
+++ vite-project/src/Component/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+    <navbar>
+        <div>
+            <h1>home</h1>
+            <h1>services</h1>
+            <h1>about</h1>
+            <h1>contact</h1>
+        </div>
+    </navbar>
+
+  )
+}
+
+export default Navbar
